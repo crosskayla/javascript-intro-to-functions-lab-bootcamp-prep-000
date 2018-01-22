5726628c9889e2d018b2eb838a4f0e444a3914e3
Kayla Cross <kac288@georgetown.edu>
2018-01-22 15:31:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function sayHiToGrandma(string){
   if (string === whisper(string)){
     return "I can't hear you!"
   }
-  else if (string === shout(string))
+  else if (string === shout(string)){
+    
+  }
 }
