827975e4db040921010fa06441b5c03f8d2884a5
Kayla Cross <kac288@georgetown.edu>
2018-01-22 15:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function whisper(string){
 function logShout(string){
   console.log(shout(string))
 }
+
+function logWhisper(string){
+  console.log(whisper(string))
+})
+
+function sayHiToGrandma(string){
+  if (string === string.whisper()){
+    return "I can't hear you!"
+  }
+  else()
+}
